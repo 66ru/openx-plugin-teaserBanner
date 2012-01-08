be5349485ebf72a50f0b3d7bb61a036d522ca4e1
m8rge <to.merge@gmail.com>
2012-01-08 12:32:51 +0600

    add flash ability + refactor

--- plugins/bannerTypeHtml/vBanner/pictureBanner.class.php
+++ plugins/bannerTypeHtml/vBanner/pictureBanner.class.php
@@ -18,96 +18,70 @@ class Plugins_BannerTypeHTML_vBanner_pictureBanner extends Plugins_BannerTypeHTM
 
 	function buildForm(&$form, &$aBanner) {
 		// remove std controls
-		$deleteId = false;
 		foreach ($form->_elements as $id=>$elm) {
-			if (is_a($elm, 'HTML_QuickForm_header') && $elm->_attributes['name'] == 'header_sql') {
-				$deleteId = $id;
+			if (in_array($elm->_attributes['name'], array('bannertext','statustext'))) {
 				unset($form->_elements[$id]);
 				unset($form->_elementIndex[ $elm->_attributes['name'] ]);
 			}
-		}
-		foreach ($form->_elements as $id=>$elm) {
-			if ($id > $deleteId)
-				unset($form->_elements[$id]);
-		}
-		foreach ($form->_elementIndex as $elm=>$id) {
-			if ($id > $deleteId)
-				unset($form->_elementIndex[$elm]);
 		}
 
 		// add my controls
 		if (isset($aBanner['parameters'])) {
 			$vastVariables = unserialize($aBanner['parameters']);
-			$aBanner = array_merge($aBanner, $vastVariables);
+			$aBanner['pict_pos'] = $vastVariables['pict_pos'];
+			$aBanner['banner_width'] = $aBanner['width'];
+			$aBanner['banner_height'] = $aBanner['height'];
+			$aBanner['width'] = $vastVariables['image_width'];
+			$aBanner['height'] = $vastVariables['image_height'];
+			$form->addElement('hidden', 'banner_width', $aBanner['banner_width']);
+			$form->addElement('hidden', 'banner_height', $aBanner['banner_height']);
 		}
 		if (empty($aBanner['pict_pos'])) {
 			$aBanner['pict_pos'] = 'left';
 		}
 
-		$form->addElement('header', 'header_b_links', "Banner picture");
-		$imageName = _getContentTypeIconImageName($aBanner['image_type']);
-		$size = _getBannerSizeText('web', $aBanner['filename']);
-		$filename = $aBanner['filename'];
-		addUploadGroup($form, $aBanner,
-			array(
-				'uploadName' => 'bannerimage',
-				'radioName' => 'replaceimage',
-				'imageName' => $imageName,
-				'fileName' => $filename,
-				'fileSize' => $size,
-				'newLabel' => $GLOBALS['strNewBannerFile'],
-				'updateLabel' => $GLOBALS['strUploadOrKeep'],
-			)
-		);
 		$picturePos[] = $form->createElement('radio', 'pict_pos', '', 'left', 'left');
 		$picturePos[] = $form->createElement('radio', 'pict_pos', '', 'right', 'right');
 		$form->addGroup($picturePos, 'VideoFormatAction', 'Picture Position', "<br/>");
 
-		$header = $form->createElement('header', 'header_b_links', "Banner text");
+		$header = $form->createElement('header', 'header_b_links', "vBanner");
 		$header->setAttribute('icon', 'icon-banner-text.gif');
 		$form->addElement($header);
-		$form->addElement('text', 'bannertext', 'Text');
+		$form->addElement('text', 'bannertext', 'Banner text');
 
-		$header = $form->createElement('header', 'header_b_links', "Banner link");
-		$header->setAttribute('icon', 'icon-banner-html.gif');
-		$form->addElement($header);
-		$form->addElement('text', 'url', $GLOBALS['strURL']);
-		$form->addElement('text', 'target', $GLOBALS['strTarget']);
+		$sizeG['banner_width'] = $form->createElement('text', 'banner_width', $GLOBALS['strWidth'].":");
+		$sizeG['banner_width']->setAttribute('onChange', 'oa_sizeChangeUpdateMessage("warning_change_banner_size");');
+		$sizeG['banner_width']->setSize(5);
 
-		$form->addElement('header', 'header_b_display', 'Banner display');
-		$sizeG['width'] = $form->createElement('text', 'width', $GLOBALS['strWidth'] . ":");
-		$sizeG['width']->setSize(5);
-		$sizeG['height'] = $form->createElement('text', 'height', $GLOBALS['strHeight'] . ":");
-		$sizeG['height']->setSize(5);
-		if (!empty($aBanner['bannerid'])) {
-			$sizeG['height']->setAttribute('onChange', 'oa_sizeChangeUpdateMessage("warning_change_banner_size");');
-			$sizeG['width']->setAttribute('onChange', 'oa_sizeChangeUpdateMessage("warning_change_banner_size");');
-		}
-		$form->addGroup($sizeG, 'size', $GLOBALS['strSize'], "&nbsp;", false);
-
-		$form->addElement('hidden', 'ext_bannertype', $this->getComponentIdentifier());
+		$sizeG['banner_height'] = $form->createElement('text', 'banner_height', $GLOBALS['strHeight'].":");
+		$sizeG['banner_height']->setAttribute('onChange', 'oa_sizeChangeUpdateMessage("warning_change_banner_size");');
+		$sizeG['banner_height']->setSize(5);
+		$form->addGroup($sizeG, 'bannersize', 'Banner Size', "&nbsp;", false);
 
 		//validation rules
 		$translation = new OX_Translation();
 		$widthRequiredRule = array($translation->translate($GLOBALS['strXRequiredField'], array($GLOBALS['strWidth'])), 'required');
+		$widthPositiveRule = array($translation->translate($GLOBALS['strXGreaterThanZeroField'], array($GLOBALS['strWidth'])), 'min', 1);
 		$heightRequiredRule = array($translation->translate($GLOBALS['strXRequiredField'], array($GLOBALS['strHeight'])), 'required');
-		$numericRule = array($GLOBALS['strNumericField'], 'numeric');
+		$heightPositiveRule = array($translation->translate($GLOBALS['strXGreaterThanZeroField'], array($GLOBALS['strHeight'])), 'min', 1);
+		$numericRule = array($GLOBALS['strNumericField'] , 'numeric');
 
-		$form->addGroupRule('size', array(
-				'width' => array($widthRequiredRule, $numericRule),
-				'height' => array($heightRequiredRule, $numericRule)
-			));
+		$form->addGroupRule('bannersize', array(
+			'banner_width' => array($widthRequiredRule, $numericRule, $widthPositiveRule),
+			'banner_height' => array($heightRequiredRule, $numericRule, $heightPositiveRule)));
+
+		$form->addElement('hidden', 'ext_bannertype', $this->getComponentIdentifier());
+
 		$form->setAttribute("onSubmit", "return max_formValidateHtml(this.banner)");
 	}
 
 	function preprocessForm($insert, $bannerid, &$aFields, &$aVariables) {
-		$this->processNewUploadedFile($aFields, $aVariables);
-
 		$aVastVariables = array();
 		$aVastVariables['pict_pos'] = $aFields['pict_pos'];
-		$aVastVariables['image_type'] = $aFields['image_type'];
-		$aVastVariables['image_width'] = $aFields['image_width'];
-		$aVastVariables['image_height'] = $aFields['image_height'];
+		$aVastVariables['image_width'] = $aVariables['width'];
+		$aVastVariables['image_height'] = $aVariables['height'];
+		$aVariables['width'] = $aFields['banner_width'];
+		$aVariables['height'] = $aFields['banner_height'];
 
 		// We serialise all the data into an array which is part of the ox_banners table.
 		// This is used by the deliveryEngine for serving ads and is faster then all joins
@@ -119,23 +93,6 @@ class Plugins_BannerTypeHTML_vBanner_pictureBanner extends Plugins_BannerTypeHTM
 		return true;
 	}
 
-	function processNewUploadedFile(&$aFields, &$aVariables) {
-		if (empty($_FILES['bannerimage']['name'])) {
-			return;
-		}
-		$oFile = OA_Creative_File::factoryUploadedFile('bannerimage');
-		checkForErrorFileUploaded($oFile);
-		$oFile->store('web'); // store file on webserver
-		$aFile = $oFile->getFileDetails();
-
-		if (!empty($aFile)) {
-			// using $aVariables here - as this is an attribute of the base class banner row
-			$aVariables['filename'] = $aFile['filename'];
-			$aFields['image_type'] = $aFile['contenttype'];
-			$aFields['image_width'] = $aFile['width'];
-			$aFields['image_height'] = $aFile['height'];
-		}
-	}
 }
 
 ?>
--- plugins/bannerTypeHtml/vBanner/pictureBanner.delivery.php
+++ plugins/bannerTypeHtml/vBanner/pictureBanner.delivery.php
@@ -2,35 +2,139 @@
 
 function Plugin_BannerTypeHTML_vBanner_pictureBanner_delivery_adRender(&$aBanner, $zoneId=0, $source='', $ct0='', $withText=false, $logClick=true, $logView=true, $useAlt=false, $loc, $referer)
 {
+	$prepend = (!empty($aBanner['prepend'])) ? $aBanner['prepend'] : '';
+	$append = (!empty($aBanner['append'])) ? $aBanner['append'] : '';
+
+	if ($aBanner['contenttype'] == 'swf')
+		$bannerText = Plugin_BannerTypeHTML_vBanner_pictureBanner_delivery_adRenderFlash($aBanner,$zoneId, $source, $ct0, $withText, $logClick, $logView, $useAlt, $loc, $referer);
+	else
+		$bannerText = Plugin_BannerTypeHTML_vBanner_pictureBanner_delivery_adRenderImage($aBanner,$zoneId, $source, $ct0, $withText, $logClick, $logView, $useAlt, $loc, $referer);
+
+	$clickUrl = _adRenderBuildClickUrl($aBanner, $zoneId, $source, $ct0, $logClick);
+	$target = !empty($aBanner['target']) ? $aBanner['target'] : '_blank';
+	$bannerText.= "<a href='$clickUrl' target='$target' class='textAnchor'>{$aBanner['bannertext']}</a>";
+
+    $bannerText = "<div style='width:{$aBanner['width']}px; height:{$aBanner['height']}px' class='vBanner'>$bannerText</div>";
+	return $prepend . $bannerText . $append;
+}
+
+function Plugin_BannerTypeHTML_vBanner_pictureBanner_delivery_adRenderImage(&$aBanner, $zoneId=0, $source='', $ct0='', $withText=false, $logClick=true, $logView=true, $useAlt=false, $richMedia=true, $loc='', $referer='', $context=array(), $useAppend=true)
+{
 	if (isset($aBanner['parameters'])) {
 		$vastVariables = unserialize($aBanner['parameters']);
 		$aBanner = array_merge($aBanner, $vastVariables);
 	}
-
-    $prepend = !empty($aBanner['prepend']) ? $aBanner['prepend'] : '';
-    $append = !empty($aBanner['append']) ? $aBanner['append'] : '';
-	$url = _adRenderBuildClickUrl($aBanner, $zoneId, $source, $ct0, $logClick);
-    $target = !empty($aBanner['target']) ? $aBanner['target'] : '_blank';
-    $bannerText = '';
-
 	$pictPos = $aBanner['pict_pos'];
 	if (!in_array($pictPos, array('left','right'))) {
 		$pictPos = 'left';
 	}
-    if (!empty($aBanner['filename'])) {
-        $imagePath = _adRenderBuildImageUrlPrefix() . "/" . $aBanner['filename'];
-	    $imageStyle = '';
-	    if ($aBanner['image_width'] && $aBanner['image_height']) {
-		    $imageStyle = "style='width:{$aBanner['image_width']}px; height:{$aBanner['image_height']}px'";
-	    }
-        $bannerText.= "<a href='$url' target='$target' style='float:$pictPos' class='imageAnchor'><img border='0' src='$imagePath' $imageStyle /></a>";
-    }
-	$bannerText.= "<a href='$url' target='$target' class='textAnchor'>{$aBanner['bannertext']}</a>";
 
-    $bannerText = "<div style='width:{$aBanner['width']}px; height:{$aBanner['height']}px' class='vBanner'>$bannerText</div>";
 	$conf = $GLOBALS['_MAX']['CONF'];
+	$aBanner['bannerContent'] = $imageUrl = _adRenderBuildFileUrl($aBanner, $useAlt);
+
+	if (!$richMedia) {
+		return _adRenderBuildFileUrl($aBanner, $useAlt);
+	}
+
+	$clickUrl = _adRenderBuildClickUrl($aBanner, $zoneId, $source, $ct0, $logClick);
+	$target = !empty($aBanner['target']) ? $aBanner['target'] : '_blank';
+	$alt = !empty($aBanner['alt']) ? $aBanner['alt'] : '';
+	$bannerText = '';
+
+	if (!empty($aBanner['filename'])) {
+		$imageStyle = '';
+		if ($aBanner['image_width'] && $aBanner['image_height']) {
+			$imageStyle = "style='width:{$aBanner['image_width']}px; height:{$aBanner['image_height']}px'";
+		}
+		$bannerText.= "<a href='$clickUrl' target='$target' style='float:$pictPos' class='imageAnchor'><img border='0' alt='$alt' title='$alt' src='$imageUrl' $imageStyle /></a>";
+	}
+
 	$beaconTag = ($logView && $conf['logging']['adImpressions']) ? _adRenderImageBeacon($aBanner, $zoneId, $source, $loc, $referer) : '';
-	return $prepend . $bannerText . $beaconTag . $append;
+	return $bannerText . $beaconTag;
+}
+
+function Plugin_BannerTypeHTML_vBanner_pictureBanner_delivery_adRenderFlash(&$aBanner, $zoneId=0, $source='', $ct0='', $withText=false, $logClick=true, $logView=true, $useAlt=false, $richMedia=true, $loc='', $referer='', $context=array())
+{
+	if (isset($aBanner['parameters'])) {
+		$vastVariables = unserialize($aBanner['parameters']);
+		$aBanner = array_merge($aBanner, $vastVariables);
+	}
+	$pictPos = $aBanner['pict_pos'];
+	if (!in_array($pictPos, array('left','right'))) {
+		$pictPos = 'left';
+	}
+
+	$conf = $GLOBALS['_MAX']['CONF'];
+	$width = !empty($aBanner['image_width']) ? $aBanner['image_width'] : 0;
+	$height = !empty($aBanner['image_height']) ? $aBanner['image_height'] : 0;
+	$pluginVersion = !empty($aBanner['pluginversion']) ? $aBanner['pluginversion'] : '4';
+	if (!empty($aBanner['alt_filename']) || !empty($aBanner['alt_imageurl'])) {
+		$altImageAdCode = Plugin_BannerTypeHTML_vBanner_pictureBanner_delivery_adRenderImage($aBanner, $zoneId, $source, $ct0, false, $logClick, false, true, true, $loc, $referer, false);
+		$fallBackLogURL = _adRenderBuildLogURL($aBanner, $zoneId, $source, $loc, $referer, '&', true);
+	} else {
+		$altImageAdCode = "<img src='" . _adRenderBuildImageUrlPrefix() . '/1x1.gif' . "' alt='".$aBanner['alt']."' title='".$aBanner['alt']."' border='0' />";
+		$fallBackLogURL = false;
+	}
+
+	// Create the anchor tag..
+	$clickUrl = _adRenderBuildClickUrl($aBanner, $zoneId, $source, $ct0, $logClick);
+	if (!empty($clickUrl)) {  // There is a link
+		$status = _adRenderBuildStatusCode($aBanner);
+		$target = !empty($aBanner['target']) ? $aBanner['target'] : '_blank';
+		$swfParams = array('clickTARGET' => $target, 'clickTAG' => $clickUrl);
+	} else {
+		$swfParams = array();
+	}
+
+	if (!empty($aBanner['parameters'])) {
+		$aAdParams = unserialize($aBanner['parameters']);
+		if (isset($aAdParams['swf']) && is_array($aAdParams['swf'])) {
+			// Converted SWF file, use paramters content
+			$swfParams = array();
+			$aBannerSwf = $aBanner;
+			// Set the flag to let _adRenderBuildClickUrl know that we're not using clickTAG
+			$aBannerSwf['noClickTag'] = true;
+			foreach ($aAdParams['swf'] as $iKey => $aSwf) {
+				$aBannerSwf['url'] = $aSwf['link'];
+				$swfParams["alink{$iKey}"] = _adRenderBuildClickUrl($aBannerSwf, $zoneId, $source, $ct0, $logClick);
+				$swfParams["atar{$iKey}"]  = $aSwf['tar'];
+			}
+		}
+	}
+	$fileUrl = _adRenderBuildFileUrl($aBanner, false);
+	$rnd = md5(microtime());
+
+	$swfId = (!empty($aBanner['alt']) ? $aBanner['alt'] : 'Advertisement');
+
+	$code = "
+<div id='ox_$rnd' style='display:inline;float:$pictPos'>$altImageAdCode</div>
+<script type='text/javascript'><!--/"."/ <![CDATA[
+    var ox_swf = new FlashObject('{$fileUrl}', '{$swfId}', '{$width}', '{$height}', '{$pluginVersion}');\n";
+	foreach ($swfParams as $key => $value) {
+		// URL encode the value, but leave any Openads "magic macros" unescaped to allow substitution
+		$code .= "    ox_swf.addVariable('{$key}', '" . preg_replace('#%7B(.*?)%7D#', '{$1}', urlencode($value)) . "');\n";
+	}
+	if (!empty($aBanner['transparent'])) {
+		$code .= "\n   ox_swf.addParam('wmode','transparent');";
+	}
+	$code .= "
+    ox_swf.addParam('allowScriptAccess','always');
+    ox_swf.write('ox_$rnd');\n";
+
+	if ($logView && $conf['logging']['adImpressions']) {
+		// Only render the log beacon if the user has the minumum required flash player version
+		$code .= "    if (ox_swf.installedVer.versionIsValid(ox_swf.getAttribute('version'))) { document.write(\""._adRenderImageBeacon($aBanner, $zoneId, $source, $loc, $referer)."\"); }";
+		// Otherwise log a fallback impression (if there is a fallback creative configured)
+		if ($fallBackLogURL) {
+			$code .= ' else { document.write("'._adRenderImageBeacon($aBanner, $zoneId, $source, $loc, $referer, $fallBackLogURL).'"); }';
+		}
+	}
+	$code .= "\n/"."/ ]]> --></script>";
+	if ($fallBackLogURL) {
+		$code .= '<noscript>' . _adRenderImageBeacon($aBanner, $zoneId, $source, $loc, $referer, $fallBackLogURL) . '</noscript>';
+	}
+
+	return $code;
 }
 
 ?>
